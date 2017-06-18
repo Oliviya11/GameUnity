77fb454c64db6feb4ddd775bc7b8fc1bd4a67350
Oliviya11 <nastyawnastya@gmail.com>
2017-06-18 15:43:22 +0300

    Add background

---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+	//int counter=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		
+			Quaternion rotationY = Quaternion.AngleAxis (1, Vector3.up);
+			transform.rotation *= rotationY;
+		
+	}
+}
